bc00dca8ba5b90d6f436f56edf927dc0c68bdc88
Yitzhakpro <isako14@gmail.com>
2024-10-27 23:30:17 +0200

    added base template

---
+++ CS2PluginStarterKit.cs
@@ -0,0 +1,24 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace CS2PluginStarterKit;
+
+public class CS2PluginStarterKit : BasePlugin
+{
+    public override string ModuleName => "CS2PluginStarterKit";
+
+    public override string ModuleVersion => "0.0.1";
+    
+    public override void Load(bool hotReload)
+    {
+        base.Load(hotReload);
+        Console.WriteLine($"{ModuleName} loaded!");
+    }
+    
+    public override void Unload(bool hotReload)
+    {
+        base.Unload(hotReload);
+        Console.WriteLine($"{ModuleName} unloaded!");
+    }
+    
+}
+
